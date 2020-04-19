65049b964d78d2f567d9d58c4ac2744dd6204622
Geoffrey Litt <gklitt@gmail.com>
2020-04-19 17:35:33 -0400

    update

--- api/sendgrid-proxy.js
+++ api/sendgrid-proxy.js
@@ -8,7 +8,8 @@ export default (req, res) => {
   );
 
   if (req.body) {
-    const sendgridData = req.body
+    console.log("request body", req.body)
+    const sendgridData = JSON.parse(req.body)
   } else {
     res.status(500).send("body payload required")
     return;
